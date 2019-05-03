48405e08d816cdc775981db8762a083a3c647b8e
Vincenzo Suraci <suraci@diag.uniroma1.it>
2019-05-03 17:42:59 +0200

    Get token, device list, stauts and on/off controls

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # hassio_shelly_cloud
-Custom component to control Shelly devices using Cloud APIs
+- A Home Assistant (HA) custom-component for Shelly devices
+
+Devices
+============
+
+The custom-component is intended to manage the [Shelly](https://shelly.cloud/) devices. 
+
+The custom-component will discover the Shelly devices associated to your Shelly account and will add switches and 
+sensors (measuring the electricity: power, voltage and current) in HA.
+  
+
+Install
+============
+
+1. Copy all the `.py` and `manifest.json` files into your `/config/custom_components/meross` folder.
+- Your configuration should look like:
+```
+config
+└── custom_components
+    └── shelly_cloud
+        └── __init__.py
+        └── manifest.json
+        └── switch.py
+        └── sensor.py
+```
+
+2. Remember to **reboot** Hassio (or Home Assistant)
+
+Dependencies
+============
+...
+
+
+Configuration
+============
+
+Add your credentials to `configuration.yaml`:
+- `username` and `password` are **mandatory**
+- `scan_interval` is **optional**. It must be a positive integer number. It represents the seconds between two consecutive scans to gather new values of Meross devices' sensors and switches. The default value is 10 seconds. 
+For example:
+```
+meross:
+  username: !secret meross_userame
+  password: !secret meross_password
+  scan_interval: 10
+```
+
+Performances
+============
+Consider that the custom-component works using a **polling strategy**: it is a time-driven not an event-driven system. 
+It means that there will be always a **small delay** between an action and the result of that action.
+
+In particular:
+- acting a on/off switch on HA should result in an (almost) instantaneous effect on the device and the Shelly mobile App;
+- acting a on/off switch on the Shelly mobile App, should result in an (almost) instantaneous effect on the device, but you have to wait up to `scan_interval` seconds before it updates on HA;
+- electricity values (power, voltage, currant) are updated approx each `scan_interval` seconds before it updates on HA;
+- unplugging a device will be detected after several `scan_interval` cycles (normally less than a minute);
+- plugging in a device will be detected within `scan_interval` seconds;
+- registering a new device (to the associated Shelly account) will be detected within `shelly_devices_scan_interval` seconds;
+- unregistering a device (from the associated Shelly account) will be detected after a HA reboot.
+
+Debug
+============
+
+- To enable debug diagnostics, add this to your `configuration.yaml`:
+```
+logger:
+  default: WARNING
+  logs:
+    shelly_sensor: DEBUG
+    shelly_switch: DEBUG
+    shelly_init: DEBUG
+```
--- __init__.py
+++ __init__.py
@@ -1,17 +1,11 @@
 from datetime import timedelta
 import logging
 import voluptuous as vol
-import threading
+import requests
+import hashlib
 
-from requests.exceptions import ConnectionError
-
-from homeassistant.core import callback
 from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD, CONF_SCAN_INTERVAL)
 from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers import discovery
-from homeassistant.helpers.dispatcher import (dispatcher_send, async_dispatcher_connect)
-from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.event import async_track_time_interval
 
 """ Setting log """
 _LOGGER = logging.getLogger('shelly_cloud_init')
@@ -19,7 +13,7 @@ _LOGGER.setLevel(logging.DEBUG)
 
 """ This is needed to ensure meross_iot library is always updated """
 """ Ref: https://developers.home-assistant.io/docs/en/creating_integration_manifest.html"""
-REQUIREMENTS = ['']
+REQUIREMENTS = []
 
 """ This is needed, it impact on the name to be called in configurations.yaml """
 """ Ref: https://developers.home-assistant.io/docs/en/creating_integration_manifest.html"""
@@ -50,9 +44,122 @@ async def async_setup(hass, config):
 
     _LOGGER.debug('async_setup() >>> STARTED')
 
-    # TODO: get token
-
+    # create ShellyCloudPlatform instance
+    hass.data[DOMAIN] = ShellyCloudPlatform(hass, config)
 
     _LOGGER.debug('async_setup() <<< TERMINATED')
 
     return True
+
+# ----------------------------------------------------------------------------------------------------------------------
+#
+# Shelly Cloud Platform
+#
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class ShellyCloudPlatform:
+
+    def __init__(self, hass, config):
+
+        # home assistant
+        self._hass = hass
+        self._config = config
+
+        # Shelly Cloud credentials
+        self._username = config[DOMAIN][CONF_USERNAME]
+        self._password = config[DOMAIN][CONF_PASSWORD]
+
+        # do login and get data (False otherwise...)
+        self._data = self.login()
+
+        # if we have data, get device list
+        if self._data:
+            self._devices = self.get_device_list()
+            if self._devices:
+                self._devices_status = self.get_devices_status()
+                _LOGGER.debug(self._devices_status)
+
+    def login(self):
+        # login url
+        url = 'https://api.shelly.cloud/auth/login'
+        # get e-mail
+        email = self._username
+        # get sha1 password
+        sha1_password = hashlib.sha1(self._password.encode('utf-8')).hexdigest()
+        # set POST https params
+        params = {'email': email, 'password': sha1_password}
+        # get response to POST request
+        response = requests.post(url, params)
+        # get dict of POST response
+        data = response.json()
+        # check if everything is Ok
+        if data['isok']:
+            # login was succesful!
+            return data['data']
+        else:
+            # print errors
+            errors = data['errors']
+            for error_title, error_message in errors.items():
+                _LOGGER.error(error_title + ' : ' + error_message)
+        return False
+
+    def get_device_list(self):
+        # device list url
+        url = 'https://shelly-2-eu.shelly.cloud/interface/device/list'
+        # set POST https params
+        params = {'Authorization': 'Bearer ' + self._data['token']}
+        # get response
+        response = requests.post(url, params)
+        # get dict of POST response
+        data = response.json()
+        # check if everything is Ok
+        if data['isok']:
+            # get_device_list was succesful!
+            return data['data']['devices']
+        else:
+            # print errors
+            errors = data['errors']
+            for error_title, error_message in errors.items():
+                _LOGGER.error(error_title + ' : ' + error_message)
+        return False
+
+    def get_devices_status(self):
+        # device list url
+        url = 'https://shelly-2-eu.shelly.cloud/device/all_status'
+        # set POST https params
+        params = {'Authorization': 'Bearer ' + self._data['token']}
+        # get response
+        response = requests.post(url, params)
+        # get dict of POST response
+        data = response.json()
+        # check if everything is Ok
+        if data['isok']:
+            # get_devices_status was succesful!
+            return data['data']['devices_status']
+        else:
+            # print errors
+            errors = data['errors']
+            for error_title, error_message in errors.items():
+                _LOGGER.error(error_title + ' : ' + error_message)
+        return False
+
+    def set_device_channel(self, id, channel, turn):
+        # control url
+        url = 'https://shelly-2-eu.shelly.cloud/device/relay/control'
+        # set POST https params
+        params = {'id': id, 'channel': channel, 'turn': turn, 'Authorization': 'Bearer ' + self._data['token']}
+        # get response
+        response = requests.post(url, params)
+        # get dict of POST response
+        data = response.json()
+        # check if everything is Ok
+        if data['isok']:
+            # set_device_channel was succesful!
+            return True
+        else:
+            # print errors
+            errors = data['errors']
+            for error_title, error_message in errors.items():
+                _LOGGER.error(error_title + ' : ' + error_message)
+        return False
